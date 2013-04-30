3763fbbdc42cd7f924c1d1c6b08b1665501efac1
stephen soltesz <stephen.soltesz@gmail.com>
2013-04-30 21:34:14 +0000

    run shaperprobe test

---
+++ shaperprobe/runtests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -z ${HOSTNAME} ] ; then
+    echo "Hi. We need 'HOSTNAME' set in the envirnment to run this test."
+    echo "Good bye."
+    exit 1
+fi
+
+BUILD_DIR=${BUILD_DIR:-$PWD}
+IP=`python -c 'import socket; print socket.gethostbyname('$HOSTNAME')'`
+$BUILD_DIR/prober -n ${IP} 
